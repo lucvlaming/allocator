d4328be88423b80505330090be790888ceed9db5
Luc Vlaming <luc@swarm64.com>
2017-04-19 20:33:20 +0200

    more tests

--- include/array.hpp
+++ include/array.hpp
@@ -6,8 +6,8 @@
 #include <cassert>
 #include <type_traits>
 
-#include "field_sizes.hpp"
 #include "field_index.hpp"
+#include "field_sizes.hpp"
 #include "struct_as_tuple.hpp"
 #include "utility.hpp"
 
@@ -17,7 +17,7 @@ namespace dod
 template <class Type, size_t Count>
 class array;
 
-template <class Type, size_t Count>
+template <class Type, class Storage, size_t Count>
 class object_container;
 
 template <class Type, size_t Count, class FieldType>
@@ -53,15 +53,16 @@ private:
     friend class array<std::remove_const_t<Type>, Count>;
 };
 
-template <class Type, size_t Count>
+template <class Type, class Storage, size_t Count>
 class object
 {
 public:
     ~object()
     {
-        if constexpr(std::is_same<Type, std::remove_const_t<Type>>::value)
-            detail::scatter_struct<Count, 0>(_index, detail::struct_as_tuple(_object), _data,
-                                          array<Type, Count>::field_sizes);
+        if
+            constexpr(!std::is_const<Type>::value)
+                    detail::scatter_struct<Count, 0>(_index, detail::struct_as_tuple(_object),
+                                                     _storage, array<Type, Count>::_field_sizes);
     }
 
     Type *operator->()
@@ -85,76 +86,78 @@ public:
     }
 
 private:
-    object(uint8_t *data, std::size_t index) : _data(data), _index(index)
+    object(Storage *storage, std::size_t index) : _storage(storage), _index(index)
     {
-        detail::gather_struct<Count, 0>(_index, detail::struct_as_tuple(_object), _data,
-                                     array<Type, Count>::field_sizes);
+        detail::gather_struct<Count, 0>(_index, detail::struct_as_tuple(_object), _storage,
+                                        array<Type, Count>::_field_sizes);
     }
 
-    Type        _object;
-    uint8_t *   _data;
-    size_t  _index;
+    Type     _object;
+    Storage *_storage;
+    size_t   _index;
 
     friend class array<std::remove_const_t<Type>, Count>;
-    friend class object_container<std::remove_const_t<Type>, Count>;
+    friend class object_container<Type, Storage, Count>;
+    friend class object_container<const Type, const Storage, Count>;
 };
 
-template <class Type, size_t Count>
+template <class Type, class Storage, size_t Count>
 class object_container
 {
 public:
     auto begin()
     {
-        return object_container<Type, Count>(data, 0);
+        return object_container<Type, Storage, Count>(_storage, 0);
     }
 
     auto begin() const
     {
-        return object_container<const Type, Count>(data, 0);
+        return object_container<const Type, const Storage, Count>(_storage, 0);
     }
 
     auto end()
     {
-        return object_container<Type, Count>(data, Count);
+        return object_container<Type, Storage, Count>(_storage, Count);
     }
 
     auto end() const
     {
-        return object_container<const Type, Count>(data, Count);
+        return object_container<const Type, const Storage, Count>(_storage, Count);
     }
 
     auto operator*()
     {
-        return object<Type, Count>(data, index);
+        return object<Type, Storage, Count>(_storage, _index);
     }
 
     auto operator*() const
     {
-        return object<const Type, Count>(data, index);
+        return object<const Type, const Storage, Count>(_storage, _index);
     }
 
     auto &operator++()
     {
-        ++index;
+        ++_index;
         return *this;
     }
 
-    auto operator==(const object_container& other) const
+    auto operator==(const object_container &other) const
     {
-        return index == other.index && data == other.data;
+        return _index == other._index && _storage == other._storage;
     }
 
-    auto operator!=(const object_container& other)  const
+    auto operator!=(const object_container &other) const
     {
         return !(operator==(other));
     }
 
 private:
-    object_container(uint8_t* d, size_t i) : data(d), index(i)
+    object_container(Storage *storage, size_t index) : _storage(storage), _index(index)
     {
     }
-    uint8_t *data;
-    size_t index;
+
+    Storage *_storage;
+    size_t   _index;
 
     friend class array<std::remove_const_t<Type>, Count>;
 };
@@ -166,70 +169,73 @@ private:
     static_assert(std::is_literal_type<Type>::value, "Type needs to be a literal type");
     static_assert(Count >= 1, "Empty array does not make sense");
 
-    static constexpr Type instance    = {};
-    static constexpr auto field_sizes = detail::field_sizes(instance);
-    static constexpr auto packed_size = detail::fold_sum(field_sizes);
+    static constexpr Type _instance    = {};
+    static constexpr auto _field_sizes = detail::field_sizes(_instance);
+    static constexpr auto _packed_size = detail::fold_sum(_field_sizes);
 
 public:
     static constexpr Type fields = {};
 
-    constexpr auto size()
+    constexpr auto size() const
     {
         return Count;
     }
 
     array()
     {
-        detail::initialize_struct<Count, 0>(detail::struct_as_tuple(fields), data.data(), field_sizes);
+        detail::initialize_struct<Count, 0>(detail::struct_as_tuple(fields), _storage.data(),
+                                            _field_sizes);
     }
 
     template <class FieldType>
     auto operator[](FieldType &_field)
     {
         using FT = typename std::remove_cv<FieldType>::type;
-        const auto index = detail::field_index(fields, &_field);
-        assert(index != std::numeric_limits<size_t>::max());
-        const auto offset = detail::sum_n_parts(field_sizes, index);
-        const std::size_t begin       = reinterpret_cast<const std::size_t>(data.data());
-        FT *              fieldOffset = reinterpret_cast<FT *>(begin + offset * Count);
-        return field<Type, Count, FT>{fieldOffset};
+        return get_field<Type, FT>(_field);
     }
 
     template <class FieldType>
     auto operator[](FieldType &_field) const
     {
         using FT = typename std::remove_cv<FieldType>::type;
-        const auto index = detail::field_index(fields, &_field);
-        assert(index != std::numeric_limits<size_t>::max());
-        const auto offset = detail::sum_n_parts(field_sizes, index);
-        const std::size_t begin       = reinterpret_cast<const std::size_t>(data.data());
-        const FT *              fieldOffset = reinterpret_cast<const FT *>(begin + offset * Count);
-        return field<const Type, Count, const FT>{fieldOffset};
+        return get_field<const Type, const FT>(_field);
     }
 
     auto transposed()
     {
-        return object_container<Type, Count>(data.data(), 0);
+        return object_container<Type, std::uint8_t, Count>(_storage.data(), 0);
     }
 
     auto transposed() const
     {
-        return object_container<const Type, Count>(data.data(), 0);
+        return object_container<const Type, const std::uint8_t, Count>(_storage.data(), 0);
     }
 
     auto transposed(std::size_t index)
     {
-        return *object_container<Type, Count>(data.data(), index);
+        return *object_container<Type, std::uint8_t, Count>(_storage.data(), index);
     }
 
     auto transposed(std::size_t index) const
     {
-        return *object_container<const Type, Count>(data.data(), index);
+        return *object_container<const Type, const std::uint8_t, Count>(_storage.data(), index);
     }
 
 private:
-    std::array<uint8_t, Count * packed_size> data;
+    template <class T, class FT, class FieldType>
+    auto get_field(FieldType &_field) const
+    {
+        const auto index = detail::field_index(fields, &_field);
+        assert(index != std::numeric_limits<size_t>::max());
+        const auto        offset      = detail::sum_n_parts(_field_sizes, index);
+        const std::size_t begin       = reinterpret_cast<const std::size_t>(_storage.data());
+        FT *              fieldOffset = reinterpret_cast<FT *>(begin + offset * Count);
+        return field<T, Count, FT>{fieldOffset};
+    }
 
-    friend class object<Type, Count>;
+    std::array<uint8_t, Count * _packed_size> _storage;
+
+    friend class object<Type, std::uint8_t, Count>;
+    friend class object<const Type, const std::uint8_t, Count>;
 };
 }
--- src/test-array.cpp
+++ src/test-array.cpp
@@ -1,6 +1,6 @@
-#include <xmmintrin.h>
 #include <array>
 #include <cstddef>
+#include <xmmintrin.h>
 
 #include "catch.hpp"
 
@@ -8,54 +8,69 @@
 
 struct DataInner
 {
-    int8_t prePadding = 1;
-    int32_t first = 15;
-    int32_t second = 8;
-    int8_t postPadding = 5;
+    int8_t  prePadding  = 1;
+    int32_t first       = 15;
+    int32_t second      = 8;
+    int8_t  postPadding = 5;
 };
 
 struct Data
 {
-    int8_t    first = 2;
+    int8_t    first  = 2;
     int64_t   second = 5;
     DataInner third;
     int64_t   fourth = 9;
-    std::array<int8_t, 9> fifth = {{2,3,5,7,11,13,19,23,29}};
-    int16_t sixth = 109;
-    __m128 seventh = {1.3, 2.2, 3.1, 4.5};
-    __m128i eighth = {11, 13};
-    __m128d ninth = {11.1, 13.1};
+    std::array<int8_t, 9> fifth = {{2, 3, 5, 7, 11, 13, 19, 23, 29}};
+    int16_t sixth   = 109;
+    __m128  seventh = {1.3, 2.2, 3.1, 4.5};
+    __m128i eighth  = {11, 13};
+    __m128d ninth   = {11.1, 13.1};
 };
 
-auto CompareAllByField = [](const auto& array, const auto& truth)
-{
-    for (const auto& m : array[array.fields.first])
-        REQUIRE(m == truth.first);
-    for (const auto& m : array[array.fields.second])
-        REQUIRE(m == truth.second);
-    for (const auto& m : array[array.fields.third])
+auto check_field = [](const auto &array, const auto &field, const auto &check) {
+    size_t count = 0;
+    for (const auto &m : array[field])
     {
-        REQUIRE(m.prePadding == truth.third.prePadding);
-        REQUIRE(m.first == truth.third.first);
-        REQUIRE(m.second == truth.third.second);
-        REQUIRE(m.postPadding == truth.third.postPadding);
+        const bool ok = check(m);
+        REQUIRE(ok);
+        count++;
     }
-    for (const auto& m : array[array.fields.fourth])
-        REQUIRE(m == truth.fourth);
-    for (const auto& m : array[array.fields.fifth])
-        REQUIRE(m == truth.fifth);
-    for (const auto& m : array[array.fields.sixth])
-        REQUIRE(m == truth.sixth);
-    for (const auto& m : array[array.fields.seventh])
-        REQUIRE(memcmp(&m, &truth.seventh, sizeof(truth.seventh)) == 0);
-    for (const auto& m : array[array.fields.eighth])
-        REQUIRE(memcmp(&m, &truth.eighth, sizeof(truth.eighth)) == 0);
-    for (const auto& m : array[array.fields.ninth])
-        REQUIRE(memcmp(&m, &truth.ninth, sizeof(truth.ninth)) == 0);
+    REQUIRE(count == array.size());
 };
 
-auto CompareAllUsingTranspose = [](const auto& elem, const auto& truth)
-{
+auto compare_all_by_field =
+        [](const auto &array, const auto &truth, const size_t &size, const void *fieldException) {
+            const auto check_field_if = [&](const auto &field, const auto &check) {
+                if (&field != fieldException)
+                    check_field(array, field, check);
+            };
+
+            REQUIRE(array.size() == size);
+
+            size_t count = 0;
+
+            check_field_if(array.fields.first, [&](const auto &m) { return m == truth.first; });
+            check_field_if(array.fields.second, [&](const auto &m) { return m == truth.second; });
+            check_field_if(array.fields.third, [&](const auto &m) {
+                return m.prePadding == truth.third.prePadding && m.first == truth.third.first
+                        && m.second == truth.third.second
+                        && m.postPadding == truth.third.postPadding;
+            });
+            check_field_if(array.fields.fourth, [&](const auto &m) { return m == truth.fourth; });
+            check_field_if(array.fields.fifth, [&](const auto &m) { return m == truth.fifth; });
+            check_field_if(array.fields.sixth, [&](const auto &m) { return m == truth.sixth; });
+            check_field_if(array.fields.seventh, [&](const auto &m) {
+                return memcmp(&m, &truth.seventh, sizeof(truth.seventh)) == 0;
+            });
+            check_field_if(array.fields.eighth, [&](const auto &m) {
+                return memcmp(&m, &truth.eighth, sizeof(truth.eighth)) == 0;
+            });
+            check_field_if(array.fields.ninth, [&](const auto &m) {
+                return memcmp(&m, &truth.ninth, sizeof(truth.ninth)) == 0;
+            });
+        };
+
+auto CompareAllUsingTranspose = [](const auto &elem, const auto &truth) {
     REQUIRE(elem->first == truth.first);
     REQUIRE(elem->second == truth.second);
     REQUIRE(elem->third.prePadding == truth.third.prePadding);
@@ -70,25 +85,69 @@ auto CompareAllUsingTranspose = [](const auto& elem, const auto& truth)
     REQUIRE(memcmp(&elem->ninth, &truth.ninth, sizeof(truth.ninth)) == 0);
 };
 
-
 TEST_CASE("Construction honors default member initializers")
 {
-    Data truth;
-    dod::array<Data, 16> array;
+    const Data truth;
+    auto *     noFieldException = reinterpret_cast<void *>(0);
 
-    SECTION("Test each field")
+    SECTION("1 element")
     {
-        CompareAllByField(array, truth);
+        const dod::array<Data, 1> array;
+        compare_all_by_field(array, truth, 1, noFieldException);
     }
+    SECTION("3 elements")
+    {
+        const dod::array<Data, 3> array;
+        compare_all_by_field(array, truth, 3, noFieldException);
+    }
+    SECTION("16 elements")
+    {
+        const dod::array<Data, 16> array;
+        compare_all_by_field(array, truth, 16, noFieldException);
+    }
+    SECTION("1023 elements")
+    {
+        const dod::array<Data, 1023> array;
+        compare_all_by_field(array, truth, 1023, noFieldException);
+    }
+    SECTION("1025 elements")
+    {
+        const dod::array<Data, 1025> array;
+        compare_all_by_field(array, truth, 1025, noFieldException);
+    }
+}
 
-    SECTION("Test transposed access")
-    {
-        for (size_t i=0; i<array.size(); ++i)
-        {
-            const auto elem = array.transposed(i);
-            CompareAllUsingTranspose(elem, truth);
-        }
-    }
+TEST_CASE("Set fields")
+{
+    const Data original;
+    dod::array<Data, 251> array;
+    size_t i = 0;
+    size_t j = 0;
+
+    const auto generate = []() {};
+
+    const auto set_field = [&](const auto &field, const auto &setter, const auto &checker) {
+        for (auto &m : array[field])
+            setter(m);
+
+        check_field(array, field, checker);
+        compare_all_by_field(array, original, 251, &array.fields.first);
+    };
+
+    set_field(array.fields.first, [&](auto &m) { m = static_cast<int8_t>(i++); },
+              [&](const auto &m) { return m == static_cast<int8_t>(j++); });
+
+    for (auto &second : array[array.fields.second])
+        second = i++;
+    check_field(array, array.fields.first,
+                [&](const auto &m) { return m == static_cast<int8_t>(j++); });
+    compare_all_by_field(array, original, 251, &array.fields.first);
+
+    for (auto &third : array[array.fields.third])
+        third = {static_cast<int8_t>(i++), static_cast<int32_t>(i++), static_cast<int32_t>(i++),
+                 static_cast<int8_t>(i++)};
+    for (auto &fourth : array[array.fields.fourth])
+        fourth = i++;
 }
 
 TEST_CASE("Transposed functionality")
@@ -98,7 +157,9 @@ TEST_CASE("Transposed functionality")
 
     SECTION("Test transposed access")
     {
-        for (size_t i=0; i<array.size(); ++i)
+        REQUIRE(array.size() == 16);
+
+        for (size_t i = 0; i < array.size(); ++i)
         {
             const auto elem = array.transposed(i);
             CompareAllUsingTranspose(elem, truth);
@@ -107,15 +168,23 @@ TEST_CASE("Transposed functionality")
 
     SECTION("Set value")
     {
-        Data newObject{3,6,{2,16,9,6},10,{{3,4,6,8,12,14,20,24,30}},110,{1.4,2.3,3.2,4.6},{12,14},{11.2,13.2}};
-        for (size_t i=0; i<array.size(); ++i)
+        Data newObject{3,
+                       6,
+                       {2, 16, 9, 6},
+                       10,
+                       {{3, 4, 6, 8, 12, 14, 20, 24, 30}},
+                       110,
+                       {1.4, 2.3, 3.2, 4.6},
+                       {12, 14},
+                       {11.2, 13.2}};
+        for (size_t i = 0; i < array.size(); ++i)
         {
             auto elem = array.transposed(i);
             std::memcpy(*elem, &newObject, sizeof(newObject));
         }
         SECTION("Check value")
         {
-            for (size_t i=0; i<array.size(); ++i)
+            for (size_t i = 0; i < array.size(); ++i)
                 CompareAllUsingTranspose(array.transposed(i), newObject);
         }
     }
